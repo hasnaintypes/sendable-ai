b0d3817c79a5f84f470ef97db3975716b73d78a1
hasnaintypes <hasnainoffice2024@gmail.com>
2026-03-11 21:40:02 +0500

    feat: add remote image patterns for optimized loading in Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,18 @@ const nextConfig: NextConfig = {
   turbopack: {
     root: path.join(__dirname, "."),
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "img.freepik.com",
+      },
+      {
+        protocol: "https",
+        hostname: "encrypted-tbn0.gstatic.com",
+      },
+    ],
+  },
   reactStrictMode: false,
 };
 
